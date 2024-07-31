4fee0c48cfc51630ab1c48734746f4362d6db3e2
AhmedLotfy8 <ahmedfert45@gmail.com>
2024-07-31 18:14:12 +0200

    Third commit

--- Script/script.js
+++ Script/script.js
@@ -1,17 +1,45 @@
-$('.singerNumber').click(function(){
+// Scrolling animation
+
+// $("#NavBar .navMenu a").click(function(){
+
+//     var sectionId = $(this).attr("href");
+//     var positionOfSection = $(sectionId).offset().top;
+                                                // 2 seconds of waiting before the animation starts, not 2 seconds of animation duration
+//     $("html , body").animate({scrollTop:positionOfSection},2000);
+// })
+
+//Singers
+$(document).ready(function () {
+    $('.singerDetails1').slideDown(500)
+})
+
+$('.singerNumber').click(function () {
     $('.singerDetails').not($(this).next()).slideUp(500);
     $(this).next().slideToggle(500);
 });
 
-$("#leftMenu a").click(function(){
-    
-    var sectionId= $(this).attr("href");
-    
-    var positionOfSection = $(sectionId).offset().top;
-    
-    $("html , body").animate({scrollTop:positionOfSection},2000);
-    
-})
+
+
+// Counter
+var countDownDate = new Date("august 25, 2024 15:37:25").getTime();
+setInterval(function () {
+
+    var now = new Date().getTime();
+
+    var distance = countDownDate - now;
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var mins = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var secs = Math.floor((distance % (1000 * 60)) / 1000);
+
+    $(".day").html(`${days} D`);
+    $(".hour").html(`${hours} h`);
+    $(".minute").html(`${mins} m`);
+    $('.second').html(`${secs} s`)
+
+
+}, 1000);
 
 
 // Character counter
@@ -53,3 +81,5 @@ $('.nav .navMenu i').on('click', function () {
     $('.nav .navBtn').css('transform', 'translateX(0)');
 
 })
+
+setInterval()
